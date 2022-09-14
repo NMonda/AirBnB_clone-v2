892f7663b0ca1a6c8454157ff75bf0e215410f34
Mtsumi <mtsumijr@gmail.com>
2022-09-14 14:11:31 +0300

    setting up database and user

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,15 +2,7 @@
 -- create a database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- create a user and grant privileges
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
